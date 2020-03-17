d4bc497dd2cc119bf4387c7e7ba42b9e10f91f70
brandonocasey <brandonocasey@gmail.com>
2020-03-17 17:15:01 -0400

    only update in one place

--- src/js/control-bar/progress-control/progress-control.js
+++ src/js/control-bar/progress-control/progress-control.js
@@ -2,8 +2,6 @@
  * @file progress-control.js
  */
 import Component from '../../component.js';
-import * as Dom from '../../utils/dom.js';
-import clamp from '../../utils/clamp.js';
 import {bind, throttle, UPDATE_REFRESH_INTERVAL} from '../../utils/fn.js';
 
 import './seek-bar.js';
@@ -60,31 +58,7 @@ class ProgressControl extends Component {
     if (!seekBar) {
       return;
     }
-
-    const playProgressBar = seekBar.getChild('playProgressBar');
-    const mouseTimeDisplay = seekBar.getChild('mouseTimeDisplay');
-
-    if (!playProgressBar && !mouseTimeDisplay) {
-      return;
-    }
-
-    const seekBarEl = seekBar.el();
-    const seekBarRect = Dom.getBoundingClientRect(seekBarEl);
-    let seekBarPoint = Dom.getPointerPosition(seekBarEl, event).x;
-
-    // The default skin has a gap on either side of the `SeekBar`. This means
-    // that it's possible to trigger this behavior outside the boundaries of
-    // the `SeekBar`. This ensures we stay within it at all times.
-    seekBarPoint = clamp(0, 1, seekBarPoint);
-
-    if (mouseTimeDisplay) {
-      mouseTimeDisplay.update(seekBarRect, seekBarPoint);
-    }
-
-    if (playProgressBar) {
-      playProgressBar.update(seekBarRect, seekBar.getProgress());
-    }
-
+    seekBar.update(event);
   }
 
   /**
